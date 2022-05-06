735cd75efa68835f266d8f7bf740eba126d1bd2e
alokkusingh <alok.ku.singh@gmail.com>
2022-05-06 12:31:00 +0530

    Added month expense bar chart

--- src/expenseMonthBarChart.js
+++ src/expenseMonthBarChart.js
@@ -38,11 +38,6 @@ function ExpenseMonthBarChart({ data }) {
   console.log(maxDate);
   console.log(maxExpenseAmount);
 
-  data.forEach( record => {
-          console.log("Date: " + record.date);
-      }
-      );
-
   useEffect(() => {
       drawChart();
   }, [data]);
@@ -53,9 +48,50 @@ function ExpenseMonthBarChart({ data }) {
       const margin = { top: 0, right: 10, bottom: 80, left: 30 };
 
       const colorScale = d3.scaleLinear()
-        .domain([0,5000,10000])
+        .domain([0,5000,15000])
         .range(['green', 'orange', 'red'])
         .clamp(true)
+
+
+      const parseD3Time = d3.timeFormat('%d')
+
+      const getDaysOfMonthDomain = function() {
+            const today = new Date();
+            const month = d3.timeDay(today).getMonth();
+            const year = d3.timeDay(today).getFullYear();
+            const numberOfDays = getNumberOfDaysInMonth(today);
+
+            const days = [];
+
+            for (let day = 1; day <= numberOfDays; day++) {
+               days.push(parseD3Time(new Date(year, month, day)));
+            }
+
+            console.log("days: " + days)
+
+            return days;
+      }
+
+      const getNumberOfDaysInMonth = function(date) {
+        const month = d3.timeDay(date).getMonth() + 1;
+
+        if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+            return 31;
+        }
+
+        if (month === 4 || month === 6 || month === 9 || month === 11) {
+            return 30;
+        }
+
+        const year = d3.timeDay(date).getFullYear();
+
+        if (year % 4 === 0) {
+            return 29;
+        }
+
+        return 28;
+      }
+
 
      // Remove the old svg
      d3.select('#month-exp-bar-container')
@@ -73,8 +109,9 @@ function ExpenseMonthBarChart({ data }) {
 
      // Setting the scale
     const xScale = d3.scaleBand()
-         .domain(dataArr.map((record) => record.date))
-         .range([width, 0])
+         //.domain(dataArr.map((record) => record.date))
+         .domain(getDaysOfMonthDomain())
+         .range([0, width])
          .padding(0.1);
 
     const yScale = d3.scaleLinear()
@@ -83,7 +120,8 @@ function ExpenseMonthBarChart({ data }) {
 
     // Setting up the axis
     const xAxis = d3.axisBottom(xScale)
-       .ticks(d3.timeDay, 1).tickFormat(d3.timeFormat('%d'))
+       //.ticks(d3.timeDay, 1).tickFormat(d3.timeFormat('%d'))
+       .ticks(d3.timeDay, 1)
 
     const yAxis = d3
        .axisLeft(yScale)
@@ -146,12 +184,12 @@ function ExpenseMonthBarChart({ data }) {
         .append("rect")
             .attr("class", "bar")
             .attr("fill", function(d) { return colorScale(d.amount); })
-            .attr("x", function(d) { return xScale(d.date); })
+            .attr("x", function(d) { return xScale(parseD3Time(d.date)); })
             .attr("y", function(d) { return yScale(d.amount); })
             .attr("width", xScale.bandwidth())
             .attr("height", (d) => yScale(0) - yScale(d.amount))
 
-
+      console.log("timeMonth: " + getDaysOfMonthDomain);
     }
 
   return <div id="month-exp-bar-container" />;
