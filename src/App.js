0f7febf8706ba647ec0cec0f0c01fbf67f019225
alokkusingh <alok.ku.singh@gmail.com>
2023-01-07 10:04:39 +0530

    Added Google Sign In

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import TransactionList from './TransactionList';
-import ExpenseList from './ExpenseList';
-import Salary from './Salary'
-import UploadFile from './UploadFile'
-import MenuBarWithContent from './MenuBarWithContent'
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
-class App extends Component {
-  render() {
+function App() {
+
+  const [ profile, setProfile ] = useState([]);
+  console.log("Loading App");
+
+  const clientId = '391658724084-tkoc08taco4bbt5dpru2cdc0b97aa07p.apps.googleusercontent.com';
+  useEffect(() => {
+      const initClient = () => {
+          gapi.client.init({
+              clientId: clientId,
+              scope: 'openid'
+          });
+      };
+      gapi.load('client:auth2', initClient);
+  }, []);
+
+   const onSuccess = (res) => {
+      console.log('success', res);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + res.tokenObj.id_token);
+
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+
+        fetch("/home/auth/google/validate/id-token", requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Data: " );
+            console.log(data);
+            if (data.id !== undefined) {
+              alert("Welcome " + res.profileObj.name + "!");
+              setProfile(res.profileObj);
+            } else {
+              logOut();
+              alert(res.profileObj.name + " you are not authorize to access this page please contact Alok!");
+            }
+          })
+          .catch(error => {
+            console.log('error', error);
+            logOut();
+            alert("There was some error please try after sometime!");
+          });
+   };
+
+    const onFailure = (err) => {
+        alert("Logged in Failed!");
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        console.log("Logged out: " + profile.name);
+        setProfile(null);
+        gapi.auth2.getAuthInstance().disconnect();
+    };
+
+    if(profile === undefined || profile.length === 0) {
+      return <div className="center">
+          <GoogleLogin
+              clientId={clientId}
+              buttonText="Sign in with Google"
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy={'single_host_origin'}
+              //isSignedIn={true}
+          />
+      </div>
+    } else {
+      console.log("Already Logged In");
+    }
+
     return (
         <Router>
           <Switch>
-            <Route path='/' exact={true} component={Home}/>
-            <Route path='/transactions' exact={true} component={TransactionList}/>
-            <Route path='/expenses' exact={true} component={ExpenseList}/>
-            <Route path='/salary' exact={true} component={Salary}/>
-            <Route path='/uploadFile' exact={true} component={UploadFile}/>
-            <Route path='/MenuBarWithContent' exact={true} component={MenuBarWithContent}/>
+            <Route path='/' exact={true} render={(props) => <Home {...props} clientId={clientId} logOut={logOut} />} />
           </Switch>
         </Router>
     )
-  }
 }
 
 export default App;
--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -1,26 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Navbar, NavbarBrand, NavbarText} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+import './css/App.css';
 
-export default class AppNavbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isOpen: false};
-        this.toggle = this.toggle.bind(this);
-    }
+function AppNavbar(clientId, logOut) {
 
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
-    render() {
         return <Navbar className="card-panel teal lighten-1" expand="md">
             <NavbarBrand href="/">
               <img alt="logo" src="/logo512.png" style={{ height: 40, width: 40 }} />
+              <div style={{float: 'right'}}>
+                <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} className="rightButton"/>
+              </div>
             </NavbarBrand>
             <NavbarText>Home Dashboard</NavbarText>
         </Navbar>
-    }
 }
+
+export default AppNavbar;
